4c461cc9f5e50aa89733a676b6c3a3b1cf209f0e
webdevpathologist <71933275+webdevpathologist@users.noreply.github.com>
2021-05-26 16:41:38 +0530

    Changed to  ZTM Build
    Since My Build was having design problems, Changed my build with ZTM Course build-Lesson 28

---
+++ precache-manifest.4372a1b57a7f4dcfe02da113ded8b27f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ba4ee0e0e6929f87ce668711c1fd44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15d154823b5774942ef7",
+    "url": "/static/css/main.1c1879b1.chunk.css"
+  },
+  {
+    "revision": "729c6c65f77146b3ecfd",
+    "url": "/static/js/2.984ff444.chunk.js"
+  },
+  {
+    "revision": "15d154823b5774942ef7",
+    "url": "/static/js/main.36dd9430.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
